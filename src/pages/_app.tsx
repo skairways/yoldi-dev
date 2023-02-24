35372c66d48c02675ab109eb062d3f8cd310bed4
Sultonbek <kingsulton45@gmail.com>
2023-02-24 11:22:21 +0500

    issue with layout jump fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,14 @@
 import type { AppProps } from "next/app"
-import { CacheProvider } from "@emotion/react"
-import { EmotionCache } from "@emotion/cache"
 import { Global } from "@emotion/react"
 
-import createEmotionCache from "@utility/createEmotionCache"
 import { ThemeProvider as EmotionThemeProvider } from "@/styles/theme/ThemeProvider"
 import { GlobalStyles } from "@/styles/globals"
 
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache
-}
-
-const clientSideEmotionCache = createEmotionCache()
-
-export default function App({
-  Component,
-  emotionCache = clientSideEmotionCache,
-  pageProps,
-}: MyAppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    // <CacheProvider value={emotionCache}>
     <EmotionThemeProvider>
       <Global styles={GlobalStyles} />
       <Component {...pageProps} />
     </EmotionThemeProvider>
-    // </CacheProvider>
   )
 }
