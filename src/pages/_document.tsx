68e665cd472de07fc4692c5756b7e8909d27b170
Sultonbek <kingsulton45@gmail.com>
2023-02-24 11:04:13 +0500

    fixing font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,12 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
       </Head>
